0b784eff04e0cf4f9bcfd049f7538054b6fdf0bb
oziie <29308690+oziie@users.noreply.github.com>
2017-09-25 11:55:58 +0300

    Düzeltme
    Kod düzenlemesi

--- app/src/main/java/com/example/pc/loginboshoku/MainActivity.java
+++ app/src/main/java/com/example/pc/loginboshoku/MainActivity.java
@@ -27,37 +27,17 @@ public class MainActivity extends AppCompatActivity
         et_ad= (EditText) findViewById(R.id.et_ad);
         et_sifre= (EditText) findViewById(R.id.et_sifre);
 
-        Veritabani db=new Veritabani(getApplicationContext());
-
-        /*
-        //Veritabanından verileri listeye çekiyoruz.
-        List<Bilgiler> kullanicilar=new ArrayList<Bilgiler>();
-
-        kullanicilar=db.VeriCek();
-
-        //Stringin gelişmiş hali.str=str+eklenecek kayit döngülerde değişkene eklemek için kullanılıyordu.
-        //StringBuilder daha performanslı için bunu kullandık.kullaniclar içerisindeki veriyi bu stringe aktardık.
-
-        StringBuilder stringBuilder=new StringBuilder();
-
-        for(Bilgiler bilgiler)
-
-        */
-
-
     }
 
 
     public void butonaDokunuldu(View v)
     {
 
-
-
-
+        //Kullanıcı adı ve parola alınıyor.
         String kullaniciadi = et_ad.getText().toString();
         String sifresi = et_sifre.getText().toString();
 
-
+        //Buton olayları tanımlanıyor.
         switch (v.getId())
         {
             case R.id.btn_gir:
@@ -69,12 +49,15 @@ public class MainActivity extends AppCompatActivity
 
                 Veritabani db=new Veritabani(this);
 //Sadece kullanıcı adını alıyor ve bu kullanıcının şifresini kontrol ediyor.Sonuca göre mesaj verdiriyor.
-                //Kullanıcı adı ve şifrenin ikisi de kontrol edilmesine gerek var mı ????
+               
                 String kontrol=db.KaydiKontrolEt(kullaniciadi);
-//Toast.makeText(MainActivity.this, ko, Toast.LENGTH_SHORT).show();
-                if(sifresi.equals(kontrol))
+                
+               
+               if(sifresi.equals(kontrol))
                 {
                     Toast.makeText(MainActivity.this, "Giriş yapıldı...", Toast.LENGTH_SHORT).show();
+
+                //Şifre doğruysa "Hoşgeldiniz" Intentine geçiliyor.
                     Intent girisekrani=new Intent(getApplicationContext(),Hosgeldiniz.class);
                     startActivity(girisekrani);
                 }
@@ -89,28 +72,20 @@ public class MainActivity extends AppCompatActivity
 
             break;
 
-
             case R.id.btn_kayit:
-
+    
+                //Kayıt işlemi için kayıt ol Intentine geçiş yapılıyor.
                 Intent intent=new Intent(getApplicationContext(),KayitOl.class);
                 startActivity(intent);
 
-
             case R.id.btn_temizle:
-
+            
+            //Temizleme butonu için tanımlama yapılıyor.
                 et_ad.getText().clear();
                 et_sifre.getText().clear();
 
                 break;
-
-
+                
         }
-
-
-
-
     }
-
-
-
 }
